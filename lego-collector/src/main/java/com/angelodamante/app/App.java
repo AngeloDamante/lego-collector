7e949a2012a8ebf6f6c21ec6b1407c08a693e91c
AngeloDamante <angelo.damante16@gmail.com>
2023-09-22 09:56:01 +0200

    maven project created

---
+++ lego-collector/src/main/java/com/angelodamante/app/App.java
@@ -0,0 +1,13 @@
+package com.angelodamante.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
