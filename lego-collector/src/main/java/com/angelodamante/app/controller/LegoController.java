cb139a255720343a93b56d660f409b188eadabce
Angelo D'Amante <angelo.damante16@gmail.com>
2023-10-14 19:44:08 +0200

    Added mutation tests with PIT (#3)
    added view and repository

--- lego-collector/src/main/java/com/angelodamante/app/controller/LegoController.java
+++ lego-collector/src/main/java/com/angelodamante/app/controller/LegoController.java
@@ -1,13 +1,22 @@
 package com.angelodamante.app.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.angelodamante.app.model.LegoEntity;
+import com.angelodamante.app.repository.LegoRepository;
+import com.angelodamante.app.view.LegoView;
 
 public class LegoController {
+	private LegoRepository legoRepository;
+	private LegoView legoView;
+	
+	public LegoController(LegoRepository legoRepository, LegoView legoView) {
+		this.legoRepository = legoRepository;
+		this.legoView = legoView;
+	}
 
-	public List<LegoEntity> getLegos() {
-		return new ArrayList<LegoEntity>();
+	public void allLegos() {
+		List<LegoEntity> legos = legoRepository.getLegos();
+		legoView.showAllLegos(legos);
 	}
 }
--- lego-collector/src/test/java/com/angelodamante/app/controller/LegoControllerTest.java
+++ lego-collector/src/test/java/com/angelodamante/app/controller/LegoControllerTest.java
@@ -1,18 +1,29 @@
 package com.angelodamante.app.controller;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.angelodamante.app.model.LegoEntity;
+import com.angelodamante.app.repository.LegoRepository;
+import com.angelodamante.app.view.LegoView;
+
 public class LegoControllerTest {
 
+	private LegoRepository legoRepository;
+	private LegoView legoView;
 	private LegoController legoController;
 
 	@Before
 	public void setUp() throws Exception {
-		legoController = new LegoController();
+		legoRepository = mock(LegoRepository.class);
+		legoView = mock(LegoView.class);
+		legoController = new LegoController(legoRepository, legoView);
 	}
 
 	@After
@@ -20,8 +31,13 @@ public class LegoControllerTest {
 	}
 
 	@Test
-	public void testGetLegosWhenNoLegoAvailable() {
-		assertThat(legoController.getLegos()).isEmpty();
-	}
+	public void testGetLegosOne() {
+		List<LegoEntity> legos = new ArrayList<LegoEntity>();
+		legos.add(new LegoEntity("foo"));
 
+		when(legoRepository.getLegos()).thenReturn(legos);
+
+		legoController.allLegos();
+		verify(legoView).showAllLegos(legos);
+	}
 }
